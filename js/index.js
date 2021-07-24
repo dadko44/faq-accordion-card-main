e667d7944609b95ff8e8f52b2de29e616b5ff25d
dadko44 <dawid.kuczer.info@gmail.com>
2021-07-24 23:46:45 +0200

    mobile design finished

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+let animateArrow = function(arrowElement, activate){
+    if(activate){
+        arrowElement.style.transform = 'rotate(180deg)';
+    }
+    else{
+        arrowElement.style.transform = 'rotate(0deg)'
+    }
+}
+
+let toggleAnswers = function(e){
+    e.classList.toggle('selected-question');
+    let answerElement = e.nextElementSibling;
+    if(answerElement.style.maxHeight){
+        answerElement.style.maxHeight = null;
+        answerElement.style.margin= '0 0 1rem 0';
+        animateArrow(e.children[0], false);
+    }
+    else{
+        answerElement.style.maxHeight = (answerElement.scrollHeight / parseFloat((getComputedStyle(document.documentElement).fontSize)) + 'rem');
+        animateArrow(e.children[0], true);
+        answerElement.style.margin = '0.7rem 0 1rem 0';
+    }
+}
+
